95b4300f2b0c10b5817afe9b40a511cae1cb1687
Darecoco <whaqlrh37@naver.com>
2024-08-13 09:55:03 +0900

    제너릭 실습1

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace CSAdv
 {
+    class Wanted<T> //Generic T -> U -> V ....
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            Value = value;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("String");
+            Wanted<int> wantedInt = new Wanted<int>(9090);
+            Wanted<double> wantedDouble = new Wanted<double>(21.457);
+
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
